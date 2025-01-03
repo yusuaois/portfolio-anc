b9445d80093f804022d4dfe5925e7ef47216c5ae
AnchialC <yusuaois@outlook.com>
2025-01-03 16:57:05 +0800

    Skills ✔

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client"; // 只在客户端渲染
+
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 export default function Home() {
   return (
     <div
@@ -28,8 +31,14 @@ export default function Home() {
       </section>
 
       {/* Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
       {/* Projects */}
+      <section>
+        <Projects />
+      </section>
 
       {/* Contact Me */}
     </div>
---
+++ components/Skill.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {
+  directionLeft?: boolean; // if true, skill will be on the left side
+};
+
+export default function Skill({ directionLeft }: Props) {
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.img
+        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        src="https://download.logo.wine/logo/C%2B%2B/C%2B%2B-Logo.wine.png"
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 
+        xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+      />
+      <div
+        className="absolute opacity-0 group-hover:opacity-80 transition duration-300
+       ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 
+       rounded-full z-0"
+      >
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+      </div>
+    </div>
+  );
+}
