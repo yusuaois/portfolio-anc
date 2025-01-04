0b41ee842ba171d811b992e9809f59e6ef2929c3
AnchialC <yusuaois@outlook.com>
2025-01-04 17:06:05 +0800

    Footer & Logo ✔

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
+import Logo from "@/public/img/Logo.jpg"
 export default function Home() {
   return (
     <div
@@ -46,6 +48,17 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      {/* Footer */}
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+            className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+            src={Logo.src} alt="" />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 }
