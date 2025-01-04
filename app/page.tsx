641dd608239ee418616ac35abffeefe4d591500c
AnchialC <yusuaois@outlook.com>
2025-01-04 16:04:33 +0800

    Customer Scroll 1

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div
       className="bg-[rgb(36,36,36)] text-white h-screen w-screen snap-y snap-mandatory 
-    overflow-scroll z-0"
+    overflow-y-scroll overflow-x-hidden z-0 "
     >
       {/* Header */}
       <Header />
