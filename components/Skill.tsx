d8f4ad409b56445ca483ef8eb048acf0afc642cd
AnchialC <yusuaois@outlook.com>
2025-01-04 21:39:47 +0800

    Optimize

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { motion } from "framer-motion";
-import cppLogo from "@/public/img/cpp_logo.png"
+import cppLogo from "@/public/img/cpp_logo.png";
 type Props = {
   directionLeft?: boolean; // if true, skill will be on the left side
 };
 
 export default function Skill({ directionLeft }: Props) {
   return (
-    <div className="group relative flex cursor-pointer">
-      <motion.img
-        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
-        transition={{ duration: 1 }}
-        whileInView={{ opacity: 1, x: 0 }}
+    <motion.div
+      initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      className="group relative flex cursor-pointer"
+    >
+      <img
         src={cppLogo.src}
         className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 
         xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
@@ -25,6 +27,6 @@ export default function Skill({ directionLeft }: Props) {
           <p className="text-3xl font-bold text-black opacity-100">100%</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
