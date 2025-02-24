bd228ca000bdafce1c8d65ece52d39303f394976
AnchialC <yusuaois@outlook.com>
2025-02-24 17:07:11 +0800

    Skill

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -26,6 +26,7 @@ export default function Skill({ directionLeft, skill }: Props) {
       className="group relative flex cursor-pointer w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 "
     >
       <img
+        alt=""
         src={urlFor(skill?.image).url()}
         className="rounded-full border border-gray-500 object-cover filter group-hover:grayscale transition duration-300 ease-in-out justify-center items-center w-20 h-20 md:w-24 md:h-24 xl:w-24 xl:h-24"
       />
