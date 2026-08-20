5032bcb8cd822334af70cf4182924b1ce805bd06
AnchialC <yusuaois@outlook.com>
2026-08-20 23:33:53 +0200

    重构 env.ts，移除 assertValue 函数和相关环境变量检查，简化数据集和项目 ID 的默认值设置

--- sanity/env.ts
+++ sanity/env.ts
@@ -3,22 +3,11 @@ import createImageUrlBuilder from "@sanity/image-url";
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-01-06";
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
-);
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
-);
-
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage);
-  }
-  return v;
-}
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "tulhd6kl";
 
 export const config = {
   projectId,
